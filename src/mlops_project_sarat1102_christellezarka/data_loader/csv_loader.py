1a7f62b9d4c07d886f4167976cc9b335800d8984
Christelle <christellezarka@gmail.com>
2024-12-16 19:52:47 +0200

    Add data_loader package

--- src/mlops_project_sarat1102_christellezarka/data_loader/csv_loader.py
+++ src/mlops_project_sarat1102_christellezarka/data_loader/csv_loader.py
@@ -1,6 +1,15 @@
 # src/ml_data_pipeline/data_loader/csv_loader.py
 import pandas as pd
 from .base_loader import DataLoader
+
+
 class CSVLoader(DataLoader):
     def load_data(self, file_path: str)-> pd.DataFrame:
+        """
+        Load data from a CSV file.
+        Args:
+            file_path(str): The path to the CSV file.
+        Returns:
+            pd.DataFrame: The loaded data as a DataFrame.
+        """
         return pd.read_csv(file_path)
